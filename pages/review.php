b9e8a05b4c893092461714060f28762865a6d127
Miftahul Huda <hudamiftakh8@gmail.com>
2023-06-20 13:31:14 +0700

    perbaikan

--- pages/review.php
+++ pages/review.php
@@ -48,105 +48,107 @@
             '".$id[1]."',
             '".$_REQUEST['catatan']."'
         )";
-        } 
+    } 
 
-        $exc=mysqli_query($conn,$sql);
-        if ($exc) {
-            echo '
-            <div class="alert alert-success alert-dismissible" role="alert">
-            <div class="alert-message">
-            <strong>Perhatian !! Data berhasil disimpan</strong>
-            </div>
-            </div>
+    $exc=mysqli_query($conn,$sql);
+    if ($exc) {
+        echo '
+        <div class="alert alert-success alert-dismissible" role="alert">
+        <div class="alert-message">
+        <strong>Perhatian !! Data berhasil disimpan</strong>
+        </div>
+        </div>
 
-            <meta http-equiv="refresh" content="1">
+        <meta http-equiv="refresh" content="1">
 
-            ';
-        }else{
-            echo '
-            <div class="alert alert-danger alert-dismissible" role="alert">
-            <div class="alert-message">
-            <strong>Perhatian !! Data gagal disimpan</strong>
-            </div>
-            </div>
+        ';
+    }else{
+        echo '
+        <div class="alert alert-danger alert-dismissible" role="alert">
+        <div class="alert-message">
+        <strong>Perhatian !! Data gagal disimpan</strong>
+        </div>
+        </div>
 
 
-            ';
-        }
-
+        ';
     }
 
-    $ry = mysqli_query($conn,"SELECT * FROM `m_review` WHERE kode_transaksi='".$id[0]."'");
-    $datane = mysqli_fetch_array($ry);
-    ?>
+}
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-success collapsed">
-                <div class="panel-heading">
-                        Review Pelanggan
+$ry = mysqli_query($conn,"SELECT * FROM `m_review` WHERE kode_transaksi='".$id[0]."'");
+$datane = mysqli_fetch_array($ry);
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-success collapsed">
+            <div class="panel-heading">
+                Review Pelanggan
+            </div>
+            <div class="panel-body">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="input-email" >1. Bagimana lokasi kos dekat dengan fasilitas umum ?</label>
+                        <select name="lokasi" class="form-control" style="width: 300px">
+                            <option value="">Pilih jawaban</option>
+                            <option value="1" <?php echo ($datane['lokasi']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
+                            <option value="2" <?php echo ($datane['lokasi']==2) ? "selected": ""; ?>>Tidak Setuju</option>
+                            <option value="3" <?php echo ($datane['lokasi']==3) ? "selected": ""; ?>>Netral</option>
+                            <option value="4" <?php echo ($datane['lokasi']==4) ? "selected": ""; ?>>Setuju</option>
+                            <option value="5" <?php echo ($datane['lokasi']==5) ? "selected": ""; ?>>Sangat Setuju</option>
+                        </select>
+
+                        <label for="input-email" >2. Bagimana jarak yang anda tempuh sesuai dengan aplikasi ?</label>
+                        <select name="jarak" class="form-control" style="width: 300px">
+                            <option value="">Pilih jawaban</option>
+                            <option value="1" <?php echo ($datane['jarak']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
+                            <option value="2" <?php echo ($datane['jarak']==2) ? "selected": ""; ?>>Tidak Setuju</option>
+                            <option value="3" <?php echo ($datane['jarak']==3) ? "selected": ""; ?>>Netral</option>
+                            <option value="4" <?php echo ($datane['jarak']==4) ? "selected": ""; ?>>Setuju</option>
+                            <option value="5" <?php echo ($datane['jarak']==5) ? "selected": ""; ?>>Sangat Setuju</option>
+                        </select>
+
+                        <label for="input-email" >3. Apakah pemesanan mudah dengan aplikasi ini ?</label>
+                        <select name="pesan_mudah" class="form-control" style="width: 300px">
+                            <option value="">Pilih jawaban</option>
+                            <option value="1" <?php echo ($datane['pesan_mudah']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
+                            <option value="2" <?php echo ($datane['pesan_mudah']==2) ? "selected": ""; ?>>Tidak Setuju</option>
+                            <option value="3" <?php echo ($datane['pesan_mudah']==3) ? "selected": ""; ?>>Netral</option>
+                            <option value="4" <?php echo ($datane['pesan_mudah']==4) ? "selected": ""; ?>>Setuju</option>
+                            <option value="5" <?php echo ($datane['pesan_mudah']==5) ? "selected": ""; ?>>Sangat Setuju</option>
+                        </select>
+
+
+                        <label for="input-email" >4. Apakah tampilan aplikasi ini bagus ?</label>
+                        <select name="aplikasi" class="form-control" style="width: 300px">
+                            <option value="">Pilih jawaban</option>
+                            <option value="1" <?php echo ($datane['aplikasi']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
+                            <option value="2" <?php echo ($datane['aplikasi']==2) ? "selected": ""; ?>>Tidak Setuju</option>
+                            <option value="3" <?php echo ($datane['aplikasi']==3) ? "selected": ""; ?>>Netral</option>
+                            <option value="4" <?php echo ($datane['aplikasi']==4) ? "selected": ""; ?>>Setuju</option>
+                            <option value="5" <?php echo ($datane['aplikasi']==5) ? "selected": ""; ?>>Sangat Setuju</option>
+                        </select>
+
+                        <label for="input-email" >5. Apakah UI/UX sudah sesuai ?</label>
+                        <select name="ui" class="form-control" style="width: 300px">
+                            <option value="">Pilih jawaban</option>
+                            <option value="1" <?php echo ($datane['ui']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
+                            <option value="2" <?php echo ($datane['ui']==2) ? "selected": ""; ?>>Tidak Setuju</option>
+                            <option value="3" <?php echo ($datane['ui']==3) ? "selected": ""; ?>>Netral</option>
+                            <option value="4" <?php echo ($datane['ui']==4) ? "selected": ""; ?>>Setuju</option>
+                            <option value="5" <?php echo ($datane['ui']==5) ? "selected": ""; ?>>Sangat Setuju</option>
+                        </select>
+
+                        <label for="input-email">6. Catatan</label>
+                        <textarea class="form-control" placeholder="Catatan" name="catatan" style="width: 300px; height: 100px"><?php echo $datane['catatan']; ?></textarea>
                     </div>
-                <div class="panel-body">
-                    <form action="" method="POST">
-                        <div class="form-group">
-                            <label for="input-email" >1. Bagimana lokasi kos dekat dengan fasilitas umum ?</label>
-                            <select name="lokasi" class="form-control" style="width: 300px">
-                                <option value="">Pilih jawaban</option>
-                                <option value="1" <?php echo ($datane['lokasi']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
-                                <option value="2" <?php echo ($datane['lokasi']==2) ? "selected": ""; ?>>Tidak Setuju</option>
-                                <option value="3" <?php echo ($datane['lokasi']==3) ? "selected": ""; ?>>Netral</option>
-                                <option value="4" <?php echo ($datane['lokasi']==4) ? "selected": ""; ?>>Setuju</option>
-                                <option value="5" <?php echo ($datane['lokasi']==5) ? "selected": ""; ?>>Sangat Setuju</option>
-                            </select>
-
-                            <label for="input-email" >2. Bagimana jarak yang anda tempuh sesuai dengan aplikasi ?</label>
-                            <select name="jarak" class="form-control" style="width: 300px">
-                                <option value="">Pilih jawaban</option>
-                                <option value="1" <?php echo ($datane['jarak']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
-                                <option value="2" <?php echo ($datane['jarak']==2) ? "selected": ""; ?>>Tidak Setuju</option>
-                                <option value="3" <?php echo ($datane['jarak']==3) ? "selected": ""; ?>>Netral</option>
-                                <option value="4" <?php echo ($datane['jarak']==4) ? "selected": ""; ?>>Setuju</option>
-                                <option value="5" <?php echo ($datane['jarak']==5) ? "selected": ""; ?>>Sangat Setuju</option>
-                            </select>
-
-                            <label for="input-email" >3. Apakah pemesanan mudah dengan aplikasi ini ?</label>
-                            <select name="pesan_mudah" class="form-control" style="width: 300px">
-                                <option value="">Pilih jawaban</option>
-                                <option value="1" <?php echo ($datane['pesan_mudah']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
-                                <option value="2" <?php echo ($datane['pesan_mudah']==2) ? "selected": ""; ?>>Tidak Setuju</option>
-                                <option value="3" <?php echo ($datane['pesan_mudah']==3) ? "selected": ""; ?>>Netral</option>
-                                <option value="4" <?php echo ($datane['pesan_mudah']==4) ? "selected": ""; ?>>Setuju</option>
-                                <option value="5" <?php echo ($datane['pesan_mudah']==5) ? "selected": ""; ?>>Sangat Setuju</option>
-                            </select>
-
-
-                            <label for="input-email" >4. Apakah tampilan aplikasi ini bagus ?</label>
-                            <select name="aplikasi" class="form-control" style="width: 300px">
-                                <option value="">Pilih jawaban</option>
-                                <option value="1" <?php echo ($datane['aplikasi']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
-                                <option value="2" <?php echo ($datane['aplikasi']==2) ? "selected": ""; ?>>Tidak Setuju</option>
-                                <option value="3" <?php echo ($datane['aplikasi']==3) ? "selected": ""; ?>>Netral</option>
-                                <option value="4" <?php echo ($datane['aplikasi']==4) ? "selected": ""; ?>>Setuju</option>
-                                <option value="5" <?php echo ($datane['aplikasi']==5) ? "selected": ""; ?>>Sangat Setuju</option>
-                            </select>
-
-                            <label for="input-email" >5. Apakah UI/UX sudah sesuai ?</label>
-                            <select name="ui" class="form-control" style="width: 300px">
-                                <option value="">Pilih jawaban</option>
-                                <option value="1" <?php echo ($datane['ui']==1) ? "selected": ""; ?>>Sangat Tidak Setuju</option>
-                                <option value="2" <?php echo ($datane['ui']==2) ? "selected": ""; ?>>Tidak Setuju</option>
-                                <option value="3" <?php echo ($datane['ui']==3) ? "selected": ""; ?>>Netral</option>
-                                <option value="4" <?php echo ($datane['ui']==4) ? "selected": ""; ?>>Setuju</option>
-                                <option value="5" <?php echo ($datane['ui']==5) ? "selected": ""; ?>>Sangat Setuju</option>
-                            </select>
-
-                            <label for="input-email">6. Catatan</label>
-                            <textarea class="form-control" placeholder="Catatan" name="catatan" style="width: 300px; height: 100px"><?php echo $datane['catatan']; ?></textarea>
-                        </div>
+                    <?php if(in_array($_SESSION['level'], array('2'))): ?>
                         <a href="<?php echo $base_url; ?>histori_pemesanan" class="btn btn-danger">Kembali</a>
                         <button type="submit" name="simpan" class="btn btn-primary">Simpan Review</button>
-                    </form>
-                </div>
+                    <?php endif; ?>
+                </form>
             </div>
         </div>
     </div>
+</div>
